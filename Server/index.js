ef13c0bd64c3ed92d37edea35d070b0771bf12e2
Naveen pandian <npandian515@gmail.com>
2025-07-28 21:12:21 +0530

    Fix formatting by removing extra newline in index.js

--- Server/index.js
+++ Server/index.js
@@ -43,7 +43,7 @@ app.post('/api/ask', async (req,res) => {
       res.status(500).json({ error: 'An error occurred while processing your request.' });
     }
 })
-
+  
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
