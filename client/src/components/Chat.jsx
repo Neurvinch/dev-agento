ac4e163bcdb83e180f6d846b55e2088c2055dc7e
Naveen pandian <npandian515@gmail.com>
2025-07-29 22:32:31 +0530

    Add state management to Chat component for input and messages

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,6 +1,10 @@
-  import React from 'react'
+  import React, { useState } from 'react'
   
   const Chat = () => {
+
+    const [input , setInput] = useState('');
+    const [messages, setMessages] = useState([]);
+    
     return (
       <div>Chat</div>
     )
